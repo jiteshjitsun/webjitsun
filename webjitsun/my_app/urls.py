f18715ecaf3db6c6c4581fa758cf5915dead335e
jitesh <jiteshjitsun@gmail.com>
2020-05-30 22:23:19 +0530

    initial commit

---
+++ webjitsun/my_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+from django.contrib import admin
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('new_search', views.new_search, name='new_search'),
+    #path('admin/', admin.site.url),
+]
